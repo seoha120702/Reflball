c662dcf5c690489361db8b718a463e4c46ed7ee9
seoha <156888565+seoha120702@users.noreply.github.com>
2024-03-06 21:00:05 +0900

    Add files via upload

--- 블레이드_볼_2D/script.js
+++ 블레이드_볼_2D/script.js
@@ -1,50 +1,129 @@
-var ball = document.querySelector("#ball")
-let blueP = document.querySelector("#blueP")
-let redP = document.querySelector("#redP")
-let intervalid
-var btop = 0
-var num = 10
-var blueclicked
-var redclicked
+// 요소 불러오기
+const ball = $("#ball");
+const blue_player = $("#blue_player");
+const red_player = $("#red_player");
+const blueBarrier = $("#blue_barrier");
+const redBarrier = $("#red_barrier");
+const blueColldownChecker = $("#blueCC");
+const redColldownChecker = $("#redCC");
+const counter = $('#counter');
+const result = $("h1")
+let intervalID = true;
+var btop = 20;
+var num = 0.75;
+var isballClicked = false;
+var isblueCooldown = false;
+var isredCooldown = false;
+var count = 0;
+var blueEndDurationTimeoutID;
+var redEndDurationTimeoutID;
+var blueEndCooldownTimeoutID;
+var redEndCooldownTimeoutID;
 
+const colldown = 3000;
+const duration = 1500;
+
+blueBarrier.css("display", "none");
+redBarrier.css("display", "none");
+
+// setInterver 정의
 function animate() {
-    intervalid = setInterval(ball_animate, 2000/2000);
-}
+    intervalID = setInterval(ball_animate, 10);
+};
 
+function attack(player) {
+    count += 1
+    counter.html(count);
+    num *= -1.05;
+    if (player === "blue") {
+        blueBarrier.css("display", "none");
+        ball.css("background", "red");
+        blueColldownChecker.html("");
+        isblueCooldown = false;
+        clearTimeout(blueEndDurationTimeoutID);
+        clearTimeout(blueEndCooldownTimeoutID);
+    }
+    else if (player === "red") {
+        redBarrier.css("display", "none");
+        ball.css("background", "blue");
+        redColldownChecker.html("");
+        isredCooldown = false;
+        clearTimeout(redEndDurationTimeoutID);
+        clearTimeout(redEndCooldownTimeoutID);
+    };
+};
+
+// animate 정의
 function ball_animate() {
-    if (btop > 3500) {
-        document.querySelector("h1").innerHTML = "레드 승!"
-    }
-    if (btop < -3800) {
-        document.querySelector("h1").innerHTML = "블루 승!"
-    }
-    if (btop > -100 && !blueclicked) {
-        ball.style.background = "blue"
-    }
-    if (btop < 100 && !redclicked) {
-        ball.style.background = "red"
-    }
-    ball.style.top = `${btop}px`
-    btop += num
-    console.log(btop)
-}
+    // 공 움직이기
+    btop += num;
+    ball.css("top", `${btop}%`);
+    // 블루 플레이어에게 닿았다면
+    if (btop > 80) {
+        // 쳐내지 못했다면
+        if (blueBarrier.css("display") === "none") {
+            // 레드 승
+            result.html("레드 승!");
+            clearInterval(intervalID);
+        }
+        // 쳐냈다면
+        else {
+            attack("blue")
+        };
+    };
+    // 레드 플레이어에게 닿았다면
+    if (btop < 20) {
+        // 쳐내지 못했다면
+        if (redBarrier.css("display") === "none") {
+            // 블루 승
+            result.html("블루 승!");
+            clearInterval(intervalID);
+        }
+        // 쳐냈다면
+        else {
+            attack("red")
+        };
+    };
+};
 
-ball.addEventListener('click', () => {
-    animate()
-})
+// 공 눌렀다면
+ball.click(() => {
+    // setInterval 실행
+    if (!isballClicked) animate(); isballClicked = true;
+});
 
-blueP.addEventListener('click', () => {
-    if (btop > 1000) {
-        num = -10
-        redclicked = false
-        blueclicked = true
-    }
-})
+// 블루 눌렀다면
+blue_player.click('click', () => {
+    if (!isblueCooldown) {
+        blueBarrier.css("display", "block");
+        isblueCooldown = true;
 
-redP.addEventListener('click', () => {
-    if (btop < -1000) {
-        blueclicked = false
-        redclicked = true
-        num = 10
-    }
-})
+        blueEndDurationTimeoutID = setTimeout(() => {
+            blueBarrier.css("display", "none")
+            isblueCooldown = true;
+            blueColldownChecker.html("블루 쿨타임");
+        }, duration);
+        
+        blueEndCooldownTimeoutID = setTimeout(() => {
+            isblueCooldown = false;
+            blueColldownChecker.html("");
+        }, colldown + duration);
+}});
+
+// 레드 눌렀다면
+red_player.on('click', () => {
+    if (!isredCooldown) {
+    redBarrier.css("display", "block");
+        isredCooldown = true;
+
+        redEndDurationTimeoutID = setTimeout(() => {
+            redBarrier.css("display", "none");
+            isredCooldown = true;
+            redColldownChecker.html("레드 쿨타임")
+        }, duration);
+        
+        redEndCooldownTimeoutID = setTimeout(() => {
+            isredCooldown = false;
+            redColldownChecker.html("");
+        }, colldown + duration);
+}});
